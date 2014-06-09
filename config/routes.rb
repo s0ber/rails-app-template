92751db3a793019cb351d3711a3c42a909cf9c15
Sergey <sergeyshishkalov@gmail.com>
2014-06-10 01:33:15 +0400

    Remove dashboard controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'application#show'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -54,5 +54,4 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   #
-  get 'dashboard' => 'dashboard#show'
 end
